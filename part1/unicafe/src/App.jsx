3367cf9ddabb5a71f1c3ef8b967b4eb549d36379
Karel95 <karelh2207@gmail.com>
2025-02-23 21:59:31 -0300

    1d

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import Button from './components/button';
+import Statistics from './layout/statistics';
+
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button text="Good" handleClick={() => setGood(good + 1)} />
+      <Button text="Neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="Bad" handleClick={() => setBad(bad + 1)} />
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
