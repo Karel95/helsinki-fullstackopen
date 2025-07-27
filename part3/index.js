c5093294e42fac04d93aa7d40a7be59a11be4f77
Karel95 <karelh2207@gmail.com>
2025-07-27 17:08:24 -0300

    Removed unused code

--- part3/index.js
+++ part3/index.js
@@ -8,7 +8,6 @@ const app = express()
 app.use(cors())
 app.use(express.static('phonebook-frontend'))
 app.use(express.json())
-app.use(express.static('phonebook-frontend'))
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :post')
 )
