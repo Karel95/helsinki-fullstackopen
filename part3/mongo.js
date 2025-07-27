a86fef856b29863985f01fed314023d5d552529c
Karel95 <karelh2207@gmail.com>
2025-07-27 15:15:18 -0300

    Fixed eslint issues

--- part3/mongo.js
+++ part3/mongo.js
@@ -5,11 +5,10 @@ if (process.argv.length < 3) {
   process.exit(1)
 }
 
-const password = process.argv[2]
 const name = process.argv[3]
 const number = process.argv[4] ? process.argv[4] : ''
 
-const url = `mongodb+srv://karel:<db_password>@cluster0.2svuipe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = 'mongodb+srv://karel:<db_password>@cluster0.2svuipe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
