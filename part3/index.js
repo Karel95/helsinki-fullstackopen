4c4ba4e21eb674c46b1c5e789a38152467247046
Karel95 <karelh2207@gmail.com>
2025-07-27 16:50:51 -0300

    Adding frontend

--- part3/index.js
+++ part3/index.js
@@ -8,6 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.static('frontend'))
 app.use(express.json())
+app.use(express.static('frontend'))
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :post')
 )
@@ -15,18 +16,16 @@ morgan.token('post', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ' '
 })
 
-// eslint-disable-next-line no-unused-vars
 const errorHandler = (error, request, response, next) => {
-  console.error('error.name:', error.name)
-  console.error('error.message:', error.message)
+  console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).json({ error: 'malformatted id' })
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
 
-  return response.status(500).json({ error: 'Internal server error' })
+  next(error)
 }
 
 const unknownEndpoint = (request, response) => {
